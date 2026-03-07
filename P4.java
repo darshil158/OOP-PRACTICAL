b17d619da9037301b8a51abd512393b579e69534
darshil158 <darshilrana158@gmail.com>
2026-03-07 17:00:39 +0530

    Add AIM comment for BMI calculation application
    Add a comment outlining the purpose of the BMI calculation application.

--- P4.java
+++ P4.java
@@ -1,3 +1,11 @@
+/* AIM :-
+Develop a Java application that calculates a person's Body Mass Index (BMI). The program should
+ask the user for their weight in pounds and height in inches. Convert these values to kilograms and
+meters respectively (1 pound = 0.45359237 kg, 1 inch = 0.0254 meters) and then calculate BMI
+(weight in kg / (height in meters)^2). Display the calculated BMI. */
+
+
+
 import java.util.*;
 public class P4 {
     public static void main(String[] args) {

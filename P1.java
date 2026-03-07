8091126adc4be81551eeab6cd01d3a4d3539a3fb
darshil158 <darshilrana158@gmail.com>
2026-03-07 16:58:17 +0530

    Update P1.java

--- P1.java
+++ P1.java
@@ -1,3 +1,11 @@
+/* AIM :-
+Develop a Java program that prompts the user to enter a distance in meters. Your program should
+then convert this distance to feet (1 meter = 3.28084 feet) and display the result formatted to two
+decimal places. */
+
+
+
+
 import java.util.*;
 public class P1 {
     public static void main(String args[]){

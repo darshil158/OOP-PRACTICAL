a37a6207bae044a873457e1b17f85bb8363b1a9a
darshil158 <darshilrana158@gmail.com>
2026-03-07 17:01:06 +0530

    Update P5.java

--- P5.java
+++ P5.java
@@ -1,3 +1,10 @@
+
+/* AIM :-
+Write a program that takes the lengths of three sides of a triangle as input. Calculate and print the
+area of the triangle. Ensure that the program validates if the given side lengths can actually form a
+triangle (sum of any two sides must be greater than the third side) before calculating the area (use
+Heron's formula: Area = sqrt(s*(s-a)*(s-b)*(s-c)) where s = (a+b+c)/2). */
+
 import java.util.*;
 
 public class P5 {
